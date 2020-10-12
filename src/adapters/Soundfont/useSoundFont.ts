0c5ad5af5322e35a5ea8d068e7bd012b0b7d87be
gusaiani <gustavo@poe.ma>
2020-10-12 12:12:24 -0300

    More

--- src/adapters/Soundfont/useSoundFont.ts
+++ src/adapters/Soundfont/useSoundFont.ts
@@ -1,3 +1,7 @@
+import { useState, useRef } from "react";
+import { Optional } from "../../domain/types";
+import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "../../domain/sound";
+
 interface Settings {
   AudioContext: AudioContextType;
 }
@@ -13,8 +17,40 @@ interface Adapted {
 
 export function useSoundFont({ AudioContext }: Settings): Adapted {
   let activeNodes: AudioNodesRegistry = {};
+
   const [current, setCurrent] = useState<Optional<InstrumentName>>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [player, setPlayer] = useState<Optional<Player>>(null);
   const audio = useRef(new AudioContext());
+
+  async function resume() {
+    return audio.current.state === "suspended"
+      ? await audio.current.resume()
+      : Promise.resolve();
+  }
+
+  async function load(instrument: InstrumentName = DEFAULT_INSTRUMENT) {
+    setLoading(true);
+    const player = await Soundfont.instrument(audio.current, instrument);
+
+    setLoading(false);
+    setCurrent(instrument);
+    setPlayer(player);
+  }
+
+  async function play(note: MidiValue) {
+    await resume();
+    if (!player) return;
+
+    const node = player.play(note.toString());
+    activeNodes = { ...activeNodes, [note]: node };
+  }
+
+  async function stop(note: MidiValue) {
+    await resume();
+    if (!activeNodes[note]) return;
+
+    activeNodes[note]!.stop();
+    activeNodes = { ...activeNodes, [note]: null };
+  }
 }
